bc23ebc23e248626ff80c4e6cadc8f4b3884b0de
Manan Pandya <mananppandya@gmail.com>
2019-08-16 16:22:17 +0530

    Allow to upload employee profile image
    - Added feature which allow to upload employee profile image.
    - Validate size & type of profile image.
    - Pass it to back-end to save into database (base64String).

--- EMS-Frontend/src/app/employee/add-edit-emp/add-edit-emp.component.ts
+++ EMS-Frontend/src/app/employee/add-edit-emp/add-edit-emp.component.ts
@@ -18,8 +18,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AddEditEmpComponent implements OnInit {
 
+  //#region VARIABLES
   employeeForm: FormGroup;
   submitted = false;
+
+  // Variables for Image-Upload
+  fileName;
+  private imageURL;
+  errorMessage;
+  isValidFileSize: boolean;
+  isValidFileType: boolean;
 
   private identityType: string;
   departments: Department[];
@@ -29,17 +37,19 @@ export class AddEditEmpComponent implements OnInit {
   bloodgroups: Bloodgroup[];
   private genderList: string[];
   private employeeId: number;
+  //#endregion
 
+  //#region  CONSTRUCTOR
   constructor(private defaultService: DefaultService,
     private formBuilder: FormBuilder,
     private employeeService: EmployeeService,
     private toastr: ToastrService,
     private activeRoute: ActivatedRoute) { }
+  //#endregion
 
   ngOnInit() {
     // Get EmployeeId
     this.employeeId = +this.activeRoute.snapshot.params["id"];
-    console.log(this.employeeId);
 
     // For Gender
     this.genderList = ['Male', 'Female'];
@@ -49,16 +59,20 @@ export class AddEditEmpComponent implements OnInit {
     this.bloodgroups = this.defaultService.bloodgroups;
     this.identitytypes = this.defaultService.identitytype;
 
+    this.fileName = "Choose File";
+
     // Initialize Angular Reactive Form
     this.initForm();
 
     // Get employee by Id if in Edit mode
-    if (this.employeeId > 0) 
+    if (this.employeeId > 0)
       this.onGetEmployeeById(this.employeeId);
   }
 
   // Convenience getter for easy access to form fields
   get f() { return this.employeeForm.controls; }
+
+  //#region EVENTS & METHODS
 
   // Add/Edit Employee
   onSubmit() {
@@ -67,8 +81,8 @@ export class AddEditEmpComponent implements OnInit {
     if (this.employeeForm.invalid) {
       return;
     }
-
     this.employeeForm.value.gender = this.employeeForm.value.gender.charAt(0);
+    this.employeeForm.value.image = this.imageURL;
 
     if (this.employeeId > 0) {
       this.employeeService.editEmployee(this.employeeId, this.employeeForm.value).subscribe(emp => {
@@ -79,6 +93,7 @@ export class AddEditEmpComponent implements OnInit {
       });
     }
     else {
+      //this.employeeForm.value.employeeId = 0;
       this.employeeService.addEmployee(this.employeeForm.value).subscribe(emp => {
         this.toastr.success("Employee added successfully.");
       }, error => {
@@ -130,11 +145,52 @@ export class AddEditEmpComponent implements OnInit {
     });
   }
 
-   // Initialize Form - Reactive
-   private initForm() {
+  // Validate selected file & convert file in Base64String
+  onFileSelected(event) {
+    let file: File = event.target.files[0]; // Get selected file from event
+
+    let fileType = file.type; // Get file type
+    let fileSize = file.size / Math.pow(1024, 2); // Convert filesize from bytes to MB
+    let supportedImages = ['png', 'jpg', 'jpeg', 'gif', 'tif', 'bmp']; // Declaration for supported files for profile image
+
+    let arrExtension = fileType.split('/'); // e.g. image/png, image/jpeg
+    let imgExtension = arrExtension[arrExtension.length - 1]; // Get file extension => e.g. png, jpeg
+
+    // Validate file extension
+    if (!supportedImages.includes(imgExtension)) {
+      this.isValidFileType = false;
+      this.fileName = "Choose File";
+      this.errorMessage = "Image type should be .jpg | .jpeg | .png | .gif | .tif | .bmp";
+    } else
+      this.isValidFileSize = true;
+
+    // Validate file size
+    if (fileSize > 2) {
+      this.isValidFileSize = false;
+      this.fileName = "Choose File";
+      this.errorMessage = "Filesize must be less than 2MB.";
+    } else
+      this.isValidFileType = true;
+
+    // Convert file in base64string type
+    if (this.isValidFileSize && this.isValidFileType) {
+      this.isValidFileSize = true;
+      this.isValidFileType = true;
+      this.fileName = file.name;
+
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageURL = reader.result;
+      }
+    }
+  }
+
+  // Initialize Form - Reactive
+  private initForm() {
 
     this.employeeForm = this.formBuilder.group({
-      employeeId: [],
+      employeeId: [0],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       fatherName: ['', Validators.required],
@@ -155,6 +211,6 @@ export class AddEditEmpComponent implements OnInit {
       identityType: ['', Validators.required],
       identityNumber: ['', Validators.required]
     });
-
   }
+  //#endregion
 }
